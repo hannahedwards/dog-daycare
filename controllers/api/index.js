47d99773363883fa2aaa01a067233bd17c7c202d
Ying Yang <vieyrayessenia8@gmail.com>
2023-02-13 18:55:11 -0700

    edited the reservation model, for stay length

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const reservationRoutes = require('./reservationRoutes')
+const reservationRoutes = require('./reservationRoutes');
 //http://localhost:3001/api/user
 router.use('/user', userRoutes);
 //http://localhost:3001/api/reservation
-router.use('/reservation', reservationRoutes)
+router.use('/reservation', reservationRoutes);
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 
+//http://localhost:3001/
 router.use('/', homeRoutes); //Display/HTML routes 
 //http://localhost:3001/api
 router.use('/api', apiRoutes); //CRUD operations
