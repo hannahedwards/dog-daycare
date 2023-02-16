0d926fda2de17f0f61d970374fc3da3f86d4bb26
Five Pandas <amandapaul1223@gmail.com>
2023-02-15 22:05:00 -0700

    move secret to .env

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
-
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
@@ -16,7 +15,7 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-  secret: 'Super secret secret',
+  secret: process.env.SESS_SECRET,
   cookie: {
     maxAge: 300000,
     httpOnly: true,
